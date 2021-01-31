2f2bbe15511e1b66655a9ec49a9908afaa2244e0
RiaRabanales <mrabanales@cifpfbmoll.eu>
2021-01-31 13:59:06 +0100

    Incluyo contenido aside principal

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,4 +15,21 @@ $(document).ready(() => {
     .then((data) => {
       document.querySelector("footer").innerHTML = data;
     });
+
+  cargarBotonTop();
 });
+
+function cargarBotonTop() {
+  //TODO no funciona
+  var botonTop = document.getElementById("botonTop");
+
+  //Función para que al hacer click vuelva al principio:
+  botonTop.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  });
+
+  window.onscroll = function () {
+    controlarBotonTop();
+  };
+}
